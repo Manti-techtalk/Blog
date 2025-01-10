ec0bda7ee32b87f34f2429ffe5811b4197cb9281
Manti Mokone <mokonemanti256@gmail.com>
2025-01-09 19:36:17 -0500

    Models defined

--- server/backend/api/models.py
+++ server/backend/api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=50)
+    introduction = models.CharField(max_length=100)
+    body = models.TextField(max_length=5000)
+    date_published = models.DateTimeField(auto_now_add=True)
+    picture = models.ImageField(default='fallback.png',blank=True)
+
+
+    def __str__(self):
+        return self.title
