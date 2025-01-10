34b562ab0bbb751316a75abd016b7b321082c0bb
Manti Mokone <mokonemanti256@gmail.com>
2025-01-09 20:00:00 -0500

    Views Done

--- server/backend/api/views.py
+++ server/backend/api/views.py
@@ -1,6 +1,50 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from api.serializers import ArticleSerializer
+from api.models import Article
+from rest_framework.decorators import api_view
+from rest_framework import status
+from rest_framework.response import Response
 
 # Create your views here.
+@api_view(['GET','POST'])
+def handle_request(request):
+    if request.method == 'GET':
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles,many = True)
+        return Response(serializer.data)
+    if request.method == "POST":
+        serializer = ArticleSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,status = status.HTTP_201_CREATED)
+        return 
+@api_view(['GET', 'PUT', 'DELETE'])
+def article_detail(request, pk):
+    try:
+        article = Article.objects.get(pk=pk)
+    except Article.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        # Retrieve a single article
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        # Update an article
+        serializer = ArticleSerializer(article, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        # Delete an article
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
 def api_home(request):
     return HttpResponse('<h1>APi Home Page</h1>')
